80864bc09bf0798eb680607f2ad3607349dc2bdf
Tara Cunningham <tcc78128@gmail.com>
2024-03-20 12:31:44 +0000

    Add data return to compute_data for refactoring and move graphing to main catchment_analysis script. Accidental whitespace change in models.py.

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -12,9 +12,16 @@ def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
 
     Gets all the measurement data from the CSV files in the data directory,
-    works out the mean for each day, and then graphs the standard deviation
-    of these means.
+    works out the std for each day.
     """
+
+    ### Problems with code ###
+    # Too many things done in the function - reading inputs, doing
+    # analysis, producing outputs.
+    # File specified to specifically in glob.glob and there is no
+    # visibility of that file when running it.
+    # map() used for one thing but then a for loop used elsewhere.
+    # code is not very readable - no comments.
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
@@ -27,8 +34,10 @@ def analyse_data(data_dir):
     
     daily_standard_deviation = pd.concat(daily_std_list)
 
-    graph_data = {
-        'daily standard deviation': daily_standard_deviation
-    }
+    return daily_standard_deviation
 
-    views.visualize(graph_data)
+    # graph_data = {
+    #     'daily standard deviation': daily_standard_deviation
+    # }
+
+    # views.visualize(graph_data)
