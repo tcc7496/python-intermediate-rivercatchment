8097098e7ecf98f86f905c5db39db80dc5e3476d
Ruth Dunn <ruthiedunn@hotmail.co.uk>
2024-03-20 14:34:38 +0000

    Modified compute_standard_deviation_by_data function and associated test

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -7,24 +7,36 @@ import pandas as pd
 
 from catchment import models, views
 
-
-def analyse_data(data_dir):
-    """Calculate the standard deviation by day between datasets.
-
-    Gets all the measurement data from the CSV files in the data directory,
-    works out the mean for each day, and then graphs the standard deviation
-    of these means.
-    """
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+def load_catchment_data(dir_path):
+    data_file_paths = glob.glob(os.path.join(dir_path, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
+    return list(data)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
-    
+def daily_std(data):
+    return data.groupby(data.index.date).std()
+
+def compute_standard_deviation_by_data(data):
+    daily_std_list = map(daily_std, data)
+
     daily_standard_deviation = pd.concat(daily_std_list)
-
     return daily_standard_deviation
+
+def analyse_data(data_dir):
+    data = load_catchment_data(data_dir)
+    return compute_standard_deviation_by_data(data)
+
+#def analyse_data(data_dir):
+#    """Calculate the standard deviation by day between datasets.
+#
+#    Gets all the measurement data from the CSV files in the data directory,
+#    works out the mean for each day, and then graphs the standard deviation
+#    of these means.
+#    """
+#    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+#    if len(data_file_paths) == 0:
+#        raise ValueError('No CSV files found in the data directory')
+#    data = map(models.read_variable_from_csv, data_file_paths)
+#
+#    return compute_standard_deviation_by_day(data)
