719f0500cfcdf75f5581d044da6711a7d01be06e
Katy Wiltshire <katy.wiltshire@gmail.com>
2024-03-20 14:33:48 +0000

    Add fda code new functions

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -5,32 +5,34 @@ import glob
 import os
 import pandas as pd
 
-from catchment import models, views
+from catchment import models
 
+
+def daily_std(data):
+    """Calculate the daily std of a 2d data array.
+    Index must be np.datetime64 compatible format."""
+    return data.groupby(data.index.date).std()
+
+def compute_standard_deviation_by_day(data):
+    """Calculate the standard deviation by day between datasets"""
+    daily_std_list = map(daily_std, data)  
+    return pd.concat(daily_std_list)
+
+def load_catchment_data(data_dir):
+    """Gets all the measurement data from the CSV files in the data directory"""
+    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError('No CSV files found in the data directory')
+    
+    data = map(models.read_variable_from_csv, data_file_paths)
+    return list(data)
 
 def analyse_data(data_dir):
     """Calculate the standard deviation by day between datasets.
 
     Gets all the measurement data from the CSV files in the data directory,
-    works out the mean for each day, and then graphs the standard deviation
-    of these means.
+    and works out the std for each day
     """
-    data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
+    data = load_catchment_data(data_dir)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
-    
-    daily_standard_deviation = pd.concat(daily_std_list)
-
-    return daily_standard_deviation
-
-    # graph_data = {
-    #     'daily standard deviation': daily_standard_deviation
-    # }
-
-    # views.visualize(graph_data)
+    return compute_standard_deviation_by_day(data)
