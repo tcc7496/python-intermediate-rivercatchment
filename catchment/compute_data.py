5de1bd036b8a98efa569d769ece6e51351911e36
Tara Cunningham <tcc78128@gmail.com>
2024-03-20 14:34:38 +0000

    Add test for analyse_data(). Refactor data loading.

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -22,22 +22,25 @@ def analyse_data(data_dir):
     # visibility of that file when running it.
     # map() used for one thing but then a for loop used elsewhere.
     # code is not very readable - no comments.
+    data = load_csv_catchment_data(data_dir)
+
+    return compute_standard_deviation_by_data(data)
+
+
+def compute_standard_deviation_by_data(data):
+    """Calcluate the daily standard deviation on potentially mutliple
+    data."""
+    return pd.concat(map(daily_std, data))
+
+
+def daily_std(data):
+    """Calculate the daily mean of a 2d data array.
+    Index must be np.datetime64 compatible format."""
+    return data.groupby(data.index.date).std()
+
+def load_csv_catchment_data(data_dir):
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
 
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
-    
-    daily_standard_deviation = pd.concat(daily_std_list)
-
-    return daily_standard_deviation
-
-    # graph_data = {
-    #     'daily standard deviation': daily_standard_deviation
-    # }
-
-    # views.visualize(graph_data)
+    return map(models.read_variable_from_csv, data_file_paths)
