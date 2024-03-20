06b9d22a19ff5603f622939f0eab4723a605fb2d
Ruth Dunn <ruthiedunn@hotmail.co.uk>
2024-03-20 15:43:55 +0000

    adding a class

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -19,9 +19,10 @@ def main(args):
         InFiles = [args.infiles]
     
     if args.full_data_analysis:
-        compute_data.analyse_data(os.path.dirname(InFiles[0]))
+        data_source = compute_data.CSVDataSource(os.path.dirname(InFiles[0]))
+        #compute_data.analyse_data(os.path.dirname(InFiles[0]))
 
-        daily_standard_deviation = compute_data.analyse_data(os.path.dirname(InFiles[0]))
+        daily_standard_deviation = compute_data.analyse_data(data_source)
 
         graph_data = {
             "daily standard deviation": daily_standard_deviation
--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -5,27 +5,34 @@ import glob
 import os
 import pandas as pd
 
-from catchment import models, views
+from catchment import models #, views
 
-def load_catchment_data(dir_path):
-    data_file_paths = glob.glob(os.path.join(dir_path, 'rain_data_2015*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError('No CSV files found in the data directory')
-    data = map(models.read_variable_from_csv, data_file_paths)
-    return list(data)
+class CSVDataSource:
+    """
+    Loads all the catchment CSV files within a specified directory.
+    """
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+
+    def load_catchment_data(self):
+        data_file_paths = glob.glob(os.path.join(self.dir_path, 'rain_data_2015*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError('No CSV files found in the data directory')
+        data = map(models.read_variable_from_csv, data_file_paths)
+        return list(data)
 
 def daily_std(data):
     return data.groupby(data.index.date).std()
 
-def compute_standard_deviation_by_data(data):
+def compute_standard_deviation_by_day(data):
     daily_std_list = map(daily_std, data)
 
     daily_standard_deviation = pd.concat(daily_std_list)
     return daily_standard_deviation
 
-def analyse_data(data_dir):
-    data = load_catchment_data(data_dir)
-    return compute_standard_deviation_by_data(data)
+def analyse_data(data_source):
+    data = data_source.load_catchment_data() 
+    return compute_standard_deviation_by_day(data)
 
 #def analyse_data(data_dir):
 #    """Calculate the standard deviation by day between datasets.
