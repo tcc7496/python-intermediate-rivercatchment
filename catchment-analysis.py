e5e6f189f4efdd1bfb215f1fe726795837015b77
Tara Cunningham <tcc78128@gmail.com>
2024-03-21 10:09:31 +0000

    argparse in separate function.

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -42,7 +42,9 @@ def main(args):
         
         views.visualize(view_data)
 
-if __name__ == "__main__":
+def create_argparse():
+    # have this in a function so we can create some tests.
+    # also good practise for separating concerns
     parser = argparse.ArgumentParser(
         description='A basic environmental data management system')
     
@@ -52,7 +54,13 @@ if __name__ == "__main__":
         help='Input CSV(s) containing measurement data')
 
     parser.add_argument('--full-data-analysis', action='store_true', dest='full_data_analysis')
+
+    return parser
+
+
+if __name__ == "__main__":
     
+    parser = create_argparse()
     args = parser.parse_args()
     
     main(args)
