7db6086c4d5ddd13099b93f9595de2050f2a1d8b
Katy Wiltshire <katy.wiltshire@gmail.com>
2024-03-21 10:09:46 +0000

    Add argprse as separate function

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -47,7 +47,7 @@ def main(args):
         
         views.visualize(view_data)
 
-if __name__ == "__main__":
+def create_argparse():
     parser = argparse.ArgumentParser(
         description='A basic environmental data management system')
     
@@ -57,6 +57,12 @@ if __name__ == "__main__":
         help='Input CSV(s) containing measurement data')
 
     parser.add_argument('--full-data-analysis', action='store_true', dest='full_data_analysis')
+    
+    return parser
+
+if __name__ == "__main__":
+    
+    parser = create_argparse()
     
     args = parser.parse_args()
     
